4878bb1eb3b2bbb514d42d25d526fcc8f31e2ad0
Ratik Kapoor <ratikrk@gmail.com>
2021-01-09 18:40:04 -0700

    Initial push

---
+++ main.c
@@ -0,0 +1,31 @@
+/*
+ * File:   main.c
+ * Author: ratik
+ *
+ * Created on January 8, 2021, 5:25 PM
+ */
+
+#include "mcc_generated_files/mcc.h"
+
+/*
+                         Main application
+ */
+void main(void)
+{
+    // Initialize the device
+    SYSTEM_Initialize();
+
+    // Enable the Global Interrupts
+    //INTERRUPT_GlobalInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    while (1)
+    {
+        PORTAbits.RA0 = 1;      // Turn on LED for test
+        __delay_ms(1000);       // Wait 1s
+        PORTAbits.RA0 = 0;      // Turn off LED for test
+        __delay_ms(1000);       // Wait 1s
+    }
+}
